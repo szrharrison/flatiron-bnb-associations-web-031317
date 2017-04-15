a5fa6075f7f6e1a94e3cca0302293d714fa8b986
szrharrison <s.z.r.harrison@gmail.com>
2017-04-15 14:41:41 -0400

    Done.

---
+++ db/migrate/20170415183730_change_reservations.rb
@@ -0,0 +1,10 @@
+class ChangeReservations < ActiveRecord::Migration
+  def up
+    change_column :reservations, :checkin, :datetime
+    change_column :reservations, :checkout, :datetime
+  end
+  def down
+    change_column :reservations, :checkin, :string
+    change_column :reservations, :checkout, :string
+  end
+end
